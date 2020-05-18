a3da3196453d8f9b910f162e589102e0aaa32b31
suochuanlin <suochuanlin@guazi.com>
2020-05-18 18:24:08 +0800

    Initial commit

---
+++ src/main/java/com/myooo/myooo/MyoooApplication.java
@@ -0,0 +1,13 @@
+package com.myooo.myooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyoooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myooo/myooo/MyoooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myooo.myooo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyoooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
