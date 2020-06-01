417aa75aa04d6fb816a5f37a727d80019fa0e4e7
suochuanlin <suochuanlin@guazi.com>
2020-06-01 10:38:33 +0800

    跳跃游戏

--- src/main/java/com/myooo/myooo/MyoooApplication.java
+++ src/main/java/com/myooo/myooo/MyoooApplication.java
@@ -1,13 +1,16 @@
 package com.myooo.myooo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MyoooApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyoooApplication.class, args);
+        log.info("ooo start 启动啦～");
     }
 
 }
