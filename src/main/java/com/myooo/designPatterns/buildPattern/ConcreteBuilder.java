0836bbc3210013062899a755562a57c236631ce6
suochuanlin <suochuanlin@guazi.com>
2020-08-11 17:29:30 +0800

    建造者模式

---
+++ src/main/java/com/myooo/designPatterns/buildPattern/ConcreteBuilder.java
@@ -0,0 +1,28 @@
+package com.myooo.designPatterns.buildPattern;
+
+public class ConcreteBuilder extends Builder{
+
+    private Computer computer = new Computer();
+    @Override
+    public void buildCPU() {
+        System.out.println("cpu");
+        computer.add("cpu");
+    }
+
+    @Override
+    public void buildBoard() {
+        System.out.println("board");
+        computer.add("board");
+    }
+
+    @Override
+    public void buildScreen() {
+        System.out.println("screen");
+        computer.add("board");
+    }
+
+    @Override
+    public Computer getComputer() {
+        return computer;
+    }
+}
---
+++ src/main/java/com/myooo/designPatterns/buildPattern/Director.java
@@ -0,0 +1,20 @@
+package com.myooo.designPatterns.buildPattern;
+
+public class Director {
+
+
+    public void contract() {
+        Builder builder = new ConcreteBuilder();
+        builder.buildBoard();
+        builder.buildCPU();
+        builder.buildScreen();
+        builder.getComputer().show();
+    }
+
+
+    public static void main(String[] args) {
+        Director director = new Director();
+        director.contract();
+    }
+
+}
